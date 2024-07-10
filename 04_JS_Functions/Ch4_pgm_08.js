0dbb1246c06b9c597671daff1df7352584fd3e26
divyadevig <divyadevigiri44@gmail.com>
2024-07-10 14:49:58 +0530

    Ch4_pgm_08.js

--- 04_JS_Functions/Ch4_pgm_08.js
+++ 04_JS_Functions/Ch4_pgm_08.js
@@ -6,13 +6,22 @@ displayMenu = function () {
 	console.log("Please choose an option:");
 	console.log("(1) Print log");
 	console.log("(2) Upload file");
+	console.log("(3) Submit the file");
+	console.log("(4) Reset the file");
+	console.log("(5)  Reference");
 	console.log("(9) Quit");
 };
 
 displayMenu();
 
-
-
+var displayMyMenu = function () {
+    console.log("My Menu:");
+    console.log("(1) View profile");
+    console.log("(2) Update settings");
+    console.log("(3) View messages");
+    console.log("(4) Logout");
+};
+displayMyMenu();
 /* Further Adventures
  *
  * 1) Add two more options to the displayMenu
