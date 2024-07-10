23bb6649b6a3bce1f52878ea1b7b5948bb538fda
divyadevig <divyadevigiri44@gmail.com>
2024-07-10 12:46:07 +0530

    Ch3-pgm_12.js

--- 03_JS_Objects/Ch3_pgm_12.js
+++ 03_JS_Objects/Ch3_pgm_12.js
@@ -8,8 +8,21 @@ var post = {
 	body : "You will not believe where I just woke up!! Only on a comet..."
 };
 
+console.log("Post Title: " + post.title);
+console.log("Author: " + post.author);
+console.log("Created Date: " + post.created);
 
+var secondPost = {
+	id : 2,
+	title : "Explore the world",
+	author : "Divya",
+	created : "2018-08-24",
+	body : "Today I roved around Mars and found evidence of ancient water."
+};
 
+console.log("\nSecond Post Title: " + secondPost.title);
+console.log("Author: " + secondPost.author);
+console.log("Created Date: " + secondPost.created);
 /* Further Adventures
  *
  * 1) Display some of the properties.
