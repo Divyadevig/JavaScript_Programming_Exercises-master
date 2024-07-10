3eb1798d77dfe68ee55b5b63343aa519831df89b
divyadevig <divyadevigiri44@gmail.com>
2024-07-10 15:31:01 +0530

    Ch4_pgm_12.js

--- 04_JS_Functions/Ch4_pgm_12.js
+++ 04_JS_Functions/Ch4_pgm_12.js
@@ -3,6 +3,7 @@
 var sale1;
 var sale2;
 var sale3;
+var sale4;
 var sale;
 var calculateTax;
 var displaySale;
@@ -10,17 +11,22 @@ var displaySale;
 sale1 = { price: 140, taxRate: 15 };
 sale2 = { price: 40, taxRate: 10 };
 sale3 = { price: 120, taxRate: 20 };
+sale4 = { price:150, taxRate: 30};
 
-calculateTax = function () {
-	sale.tax = sale.price * sale.taxRate / 100;
-	sale.total = sale.price + sale.tax;
+
+
+calculateTax = function (sale) {
+    sale.tax = sale.price * sale.taxRate / 100;
+    sale.total = sale.price + sale.tax;
 };
 
-displaySale = function () {
-  console.log("price = $" + sale.price);
-  console.log("tax @ " + sale.taxRate + "% = $" + sale.tax);
-  console.log("total cost = $" + sale.total);
+displaySale = function (sale) {
+    console.log("price = $" + sale.price);
+    console.log("tax @ " + sale.taxRate + "% = $" + sale.tax);
+    console.log("total cost = $" + sale.total);
 };
+
+
 
 sale = sale1;
 calculateTax();
@@ -34,7 +40,9 @@ sale = sale3;
 calculateTax();
 displaySale();
 
-
+sale = sale4;
+calculateTax();
+displaySale();
 
 /* Further Adventures
  *
