90465d38541fe7bbf5a2f783632e96f99c85ba5f
divyadevig <divyadevigiri44@gmail.com>
2024-07-10 12:28:12 +0530

    Ch3_pgm_09.js

--- 03_JS_Objects/Ch3_pgm_09.js
+++ 03_JS_Objects/Ch3_pgm_09.js
@@ -2,20 +2,30 @@
 
 var book1;
 var book2;
+var book3;
 
 book1 = {
-	title: "The Hobbit",
-	author: "J. R. R. Tolkien"
+	title : "The Hobbit",
+	author: "J. R. R. Tolkien",
+	genre : "Fantasy"
 };
 
 book2 = {
-	title: "Northern Lights",
-	author: "Philip Pullman"
+	title : "Northern Lights",
+	author: "Philip Pullman",
+	genre : "Fantasy"
 };
+
+book3 = {
+	title :"My Book",
+	author: "Divya",
+	genre : "fantasy"
+}
+
 
 console.log(book1.title + " by " + book1.author);
 console.log(book2.title + " by " + book2.author);
-
+console.log(book3.title + " by " + book3.author);
 
 
 /* Further Adventures
