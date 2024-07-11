8a7ca5582ab58edff143e246ae954f9ada6d6892
divyadevig <divyadevigiri44@gmail.com>
2024-07-11 14:54:38 +0530

    Ch5_pgm_06.js

--- 05_JS_Arguments/Ch5_pgm_06.js
+++ 05_JS_Arguments/Ch5_pgm_06.js
@@ -9,9 +9,32 @@ showSum = function (number1, number2) {
 
 showSum(30, 23);
 showSum(2.8, -5);
+showSum(56,74);
 
+showProduct = function(num1,num2){
+	var total = num1 * num2;
+	console.log("The product is" +total);
+};
 
+showProduct(10,20);
+showProduct(20,1);
+showProduct(1.3,5);
 
+showDifference = function(num1,num2){
+	var result=num1-num2;
+	console.log("The Difference is" +result);
+}
+showDifference(50,20);
+showDifference(200,300);
+showDifference(1.5,1);
+
+showQuotient = function(num1,num2){
+	var result = num1/num2;
+	console.log("The division is" +result);
+}
+showQuotient(20,10);
+showQuotient(10,2);
+showQuotient(90,103);
 /* Further Adventures
  *
  * 1) Use the showSum function to add 56 and 74.
--- 06_JS_Return_values/Ch6_pgm_01.js
+++ 06_JS_Return_values/Ch6_pgm_01.js
@@ -1,11 +1,16 @@
 // Returning a value from a function
 
 var getMessage;
+var getMyMessage;
 var response;
 
 getMessage = function () {
     return "I’m going on an adventure!";
 };
+
+getMyMessage = function(){
+    return ""
+}
 
 response = getMessage();
 
