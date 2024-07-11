047cc9cf3c24c4a777bf0cc50518ac649e905ab1
divyadevig <divyadevigiri44@gmail.com>
2024-07-11 14:21:00 +0530

    Ch5_pgm_05.js

--- 05_JS_Arguments/Ch5_pgm_05.js
+++ 05_JS_Arguments/Ch5_pgm_05.js
@@ -13,6 +13,29 @@ square(-2);
 square(1111);
 square(0.5);
 
+cube = function (numberToCube){
+  var result;
+  result = numberToCube * numberToCube * numberToCube;
+  console.log(numberToCube + " * " + numberToCube + " = "+result);
+};
+cube(11);
+cube(2);
+cube(-1);
+cube(0.6);
+
+var squareRoot;
+
+squareRoot = function (numberToSquareRoot) {
+    var result;
+    result = Math.sqrt(numberToSquareRoot);
+    console.log("The square root of " + numberToSquareRoot + " is " + result);
+};
+
+squareRoot(9);
+squareRoot(16);
+squareRoot(0.25);
+squareRoot(100);
+
 
 
 /* Further Adventures
