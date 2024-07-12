945b48443fa88a550fe4479c15d70a79ddb47023
divyadevig <divyadevigiri44@gmail.com>
2024-07-12 14:45:30 +0530

    completed

--- 07_JS_Object_Arguments/Ch7_pgm_01.js
+++ 07_JS_Object_Arguments/Ch7_pgm_01.js
@@ -1,6 +1,7 @@
 // Passing a function an object as an argument
 
 var planet1;
+var secondplanet;
 var getPlanetInfo;
 
 planet1 = {
@@ -10,13 +11,23 @@ planet1 = {
     radius: 69911,
     sizeRank: 1
 };
+secondplanet = {
+    name: "Earth",
+    position:3,
+    type: "Terrestrial",
+    radius:98745,
+    sizeRank:2
+}
 
 getPlanetInfo = function (planet) {
-    return planet.name + ": planet number " + planet.position;
+
+    return planet.name + ": planet number " + planet.position + " radius :" + planet.radius + " type: " + planet.type; 
+    
+
 };
 
 console.log(getPlanetInfo(planet1));
-
+console.log(getPlanetInfo(secondplanet));
 
 
 /* Further Adventures
