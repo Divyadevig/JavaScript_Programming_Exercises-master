b0f58c7a3c85c6b440bab9b2985cc124447ac79e
divyadevig <divyadevigiri44@gmail.com>
2024-07-12 08:51:36 +0530

    completed

--- 06_JS_Return_values/Ch6_pgm_05.js
+++ 06_JS_Return_values/Ch6_pgm_05.js
@@ -2,11 +2,11 @@
 
 var totalCost;
 
-totalCost = function (callOutCharge, costPerHour, numberOfHours) {
-	return callOutCharge + costPerHour * numberOfHours;
+totalCost = function (callOutCharge, costPerHour, numberOfHours,discounts) {
+	return callOutCharge + costPerHour * numberOfHours - discounts;
 };
 
-console.log("$" + totalCost(30, 40, 3));
+console.log("$" + totalCost(30, 40, 3,20));
 
 
 
