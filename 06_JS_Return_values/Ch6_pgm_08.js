cc49ec94f3144d84705d0abf48cf0baa63b9de2f
divyadevig <divyadevigiri44@gmail.com>
2024-07-12 12:50:01 +0530

    completed

--- 06_JS_Return_values/Ch6_pgm_08.js
+++ 06_JS_Return_values/Ch6_pgm_08.js
@@ -2,6 +2,7 @@
 
 var getPlayerHealth;
 var getPlayerPlace;
+var showPlayerInfo;
 
 getPlayerHealth = function (playerName, playerHealth) {
     return playerName + " has health " + playerHealth;
@@ -14,8 +15,12 @@ getPlayerPlace = function (playerName, playerPlace) {
 console.log(getPlayerHealth("Kandra", 50));
 console.log(getPlayerPlace("Kandra", "The Dungeon of Doom"));
 
-
-
+showPlayerInfo = function (playerName, playerHealth,playerPlace){
+   console.log(getPlayerHealth(playerName,playerHealth));
+   console.log(getPlayerPlace(playerName,playerPlace)); 
+   
+}
+showPlayerInfo("Dax",50,"The Bath");
 /* Further Adventures
  *
  * 1) Create a showPlayerInfo function with three
